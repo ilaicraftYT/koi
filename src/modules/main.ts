dbb8de2225549f70b06d1827e93f523ede7bed99
ilaicraftYT <70978042+ilaicraftYT@users.noreply.github.com>
2021-12-12 15:12:36 -0600

    :memo: Adding license

--- src/modules/main.ts
+++ src/modules/main.ts
@@ -1,1 +1,15 @@
+/*
+Koi - Complement for discord.js
+
+    Copyright (C) 2021-2022  ilai
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Affero General Public License as published
+        by the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Affero General Public License for more details.
+*/
 export { clap } from './text/clap';
